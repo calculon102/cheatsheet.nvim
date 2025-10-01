cc9e2697c0342efeece25e8fd3757ed06dbe7679
calculon102 <frank@pixelgerecht.de>
2025-10-01 22:23:36 +0200

    Add lsp keys

--- lua/cheatsheet/init.lua
+++ lua/cheatsheet/init.lua
@@ -3,6 +3,22 @@ local M = {}
 -- Define the function to read text from a file
 local function get_content()
     return [[
+== Language Server (LSP) ==
+    ctrl-] - go to definition
+    gq - format selected text or text object
+    K - display documentation of the symbol under the cursor
+    ctrl-x + ctrl-o - in insert mode, trigger code completion
+
+    grn - renames all references of the symbol under the cursor
+    gra - list code actions available in the line under the cursor
+    grr - lists all the references of the symbol under the cursor
+    gri - lists all the implementations for the symbol under the cursor
+    gO - lists all symbols in the current buffer
+    ctrl-s - in insert mode, display function signature under the cursor
+    [d - jump to previous diagnostic in the current buffer
+    ]d - jump to next diagnostic in the current buffer
+    ctrl-w + d - show error/warning message in the line under the cursor
+
 == Movement to letter ==
     f [letter] - Move to letter 
     t [letter] - Move up to letter 
